fd8a1191e74b041838ba8fcd8e0bdfa6ee994e11
Loic GUILLOIS <contact@loicguillois.fr>
2022-03-01 15:40:56 +0100

    Update formattage.js

--- src/tasks/stats/cras/cumulBydepartements/tasks/formattage.js
+++ src/tasks/stats/cras/cumulBydepartements/tasks/formattage.js
@@ -1,4 +1,4 @@
-const getListDepsFormatted = async (db, departements) => {
+const getListDepsFormatted = async (dbDatalake, departements) => {
 
   const depsList = new Map();
   //Insertion des stats par département
@@ -7,16 +7,16 @@ const getListDepsFormatted = async (db, departements) => {
     //CAS DOMs
     // eslint-disable-next-line max-len
     if ((departement['num_dep'].toString().startsWith('97') || departement['num_dep'].toString().startsWith('98')) && !departement['num_dep'].toString().startsWith('97150')) {
-      statDep = await db.collection('temporary_doms_stats_departements_cras_cumul').findOne({ '_id.departement': departement['num_dep'].toString() });
+      statDep = await dbDatalake.collection('temporary_doms_stats_departements_cras_cumul').findOne({ '_id.departement': departement['num_dep'].toString() });
     //CAS CORSE 2A
     } else if (departement['num_dep'].toString().startsWith('2A')) {
-      statDep = await db.collection('temporary_corse2a_stats_departements_cras_cumul').findOne({ '_id.departement': departement['num_dep'].toString() });
+      statDep = await dbDatalake.collection('temporary_corse2a_stats_departements_cras_cumul').findOne({ '_id.departement': departement['num_dep'].toString() });
     //CAS CORSE 2B
     } else if (departement['num_dep'].toString().startsWith('2B')) {
-      statDep = await db.collection('temporary_corse2b_stats_departements_cras_cumul').findOne({ '_id.departement': departement['num_dep'].toString() });
+      statDep = await dbDatalake.collection('temporary_corse2b_stats_departements_cras_cumul').findOne({ '_id.departement': departement['num_dep'].toString() });
     } else {
     //CAS NORMAL
-      statDep = await db.collection('temporary_others_stats_departements_cras_cumul').findOne({ '_id.departement': departement['num_dep'].toString() });
+      statDep = await dbDatalake.collection('temporary_others_stats_departements_cras_cumul').findOne({ '_id.departement': departement['num_dep'].toString() });
     }
 
     departement['count'] = statDep?.valeur ?? 0;
@@ -24,7 +24,7 @@ const getListDepsFormatted = async (db, departements) => {
   }
 
   //CAS TOMs (manuel car ne sont pas réellement des départements et ne font donc pas partis de la liste departements)
-  let statDepStMartin = await db.collection('temporary_stmartin_departements_cras_cumul').findOne({ '_id.departement': '97150' });
+  let statDepStMartin = await dbDatalake.collection('temporary_stmartin_departements_cras_cumul').findOne({ '_id.departement': '97150' });
   let stMartin = {
     num_dep: 978,
     dep_name: 'Saint-Martin',
@@ -33,7 +33,7 @@ const getListDepsFormatted = async (db, departements) => {
   };
   depsList.set(String(978), stMartin);
 
-  let statDepNouvelleCaldonie = await db.collection('temporary_doms_stats_departements_cras_cumul').findOne({ '_id.departement': '988' });
+  let statDepNouvelleCaldonie = await dbDatalake.collection('temporary_doms_stats_departements_cras_cumul').findOne({ '_id.departement': '988' });
   let nouvelleCaledonie = {
     num_dep: 988,
     dep_name: 'Nouvelle-Calédonie',
