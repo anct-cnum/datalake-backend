a3df78788ac049a41e1408297af98d0ba57b3bc0
Ornella Ourfi <ornella93@outlook.fr>
2023-01-04 14:52:20 +0100

    retour commentaire.

--- src/tasks/scripts/ajout-structureId-cras.js
+++ src/tasks/scripts/ajout-structureId-cras.js
@@ -13,17 +13,17 @@ const idConseillerDbAndDatalake = async db => {
   return result.map(e => ({ idConseillerDB: e, idConseillerDatalake: encrypt(e.toString()) }));
 };
 const idCnMatchCraSansStructureId = dbDatalake => async arrayIdConseiller => {
-  const resultDatalake = await dbDatalake.collection('cras').find('conseillerId', { 'structureId': { '$exists': false } });
+  const resultDatalake = await dbDatalake.collection('cras').distinct('conseillerId', { 'structureId': { '$exists': false } });
   const concatArray = arrayIdConseiller.map(e => e.idConseillerDatalake).concat(resultDatalake);
   return concatArray.filter((x, i, a) => a.indexOf(x) !== i); // return uniquement les values qui ont au moins 1 doublon dans le concatArray
 };
 
-// Partie 2 => find des cras concerné à updater
+// Partie 2 => find des cras concernés à mettre à jour
 const getCrasToConseiller = db => async id => await db.collection('cras').find({
   'conseiller.$id': id,
   'createdAt': { $lte: new Date('2022-12-01') }
 }).toArray();
-const getIdCrasSansId = dbDatalake => async id => await dbDatalake.collection('cras').distinct('_id',
+const getIdCrasSansStructureId = dbDatalake => async id => await dbDatalake.collection('cras').distinct('_id',
   { 'conseillerId': id, 'structureId': { '$exists': false } });
 
 // Partie 3 => ajout de la structure Id pour le CRA manquant !
@@ -43,16 +43,16 @@ execute(__filename, async ({ logger, db, dbDatalake }) => {
   const arrayIdConseiller = await idConseillerDbAndDatalake(db);
   const idConseillerMatchDatalake = await idCnMatchCraSansStructureId(dbDatalake)(arrayIdConseiller);
   const conseillerCraToUpdate = arrayIdConseiller.filter(e => idConseillerMatchDatalake.includes(e.idConseillerDatalake));
-  const conseillerCraToUpdateLimit = conseillerCraToUpdate.slice(~~program.limit);
+  const conseillerCraToUpdateLimit = idConseillerMatchDatalake.slice(~~program.limit);
   let modifiedCount = 0;
 
   try {
     let promises = [];
     conseillerCraToUpdateLimit.forEach(obj => {
       promises.push(new Promise(async resolve => {
-        const crasCNfs = await getCrasToConseiller(db)(obj.idConseillerDB);
-        const getCraSansIdToConseiller = await getIdCrasSansId(dbDatalake)(obj.idConseillerDatalake);
-        const filtreCraSansIdSA = crasCNfs.filter(e => getCraSansIdToConseiller.includes(encrypt(e._id.toString())));
+        const crasCnfs = await getCrasToConseiller(db)(obj.idConseillerDB);
+        const getCraSansStructureIdToConseiller = await getIdCrasSansStructureId(dbDatalake)(obj.idConseillerDatalake);
+        const filtreCraSansIdSA = crasCnfs.filter(e => getCraSansStructureIdToConseiller.includes(encrypt(e._id.toString())));
         for (let c of filtreCraSansIdSA) {
           await updateCraDatalake(dbDatalake)(c._id, c.structure.oid);
         }
@@ -65,6 +65,6 @@ execute(__filename, async ({ logger, db, dbDatalake }) => {
     logger.info(`Une erreur s'est produite lors de la mise à jour des CRAs (DataLake)`, error);
   }
 
-  logger.info(`${modifiedCount} / ${conseillerCraToUpdate.length} conseiller(s) concerné, ont eu des CRAs mis à jour`);
+  logger.info(`${modifiedCount} / ${conseillerCraToUpdate.length} conseiller(s) concerné(s), ont eu des CRAs mis à jour`);
 });
 
