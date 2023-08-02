2d77af09e8b43e6b821434d64c186113a382ed07
morgan <morgan@fitlab.fr>
2023-08-02 16:25:39 +0200

    dbdatalake au lieu de db

--- src/tasks/mongodbSync/countTotalConseillers.js
+++ src/tasks/mongodbSync/countTotalConseillers.js
@@ -3,9 +3,9 @@
 
 const { execute } = require('../utils');
 
-execute(__filename, async ({ logger, db, dbDatalake }) => {
-  const countConseillersSupprimes = await db.collection('conseillersSupprimes').countDocuments({});
-  const countConseillers = await db.collection('conseillers').countDocuments({});
+execute(__filename, async ({ logger, dbDatalake }) => {
+  const countConseillersSupprimes = await dbDatalake.collection('conseillersSupprimes').countDocuments({});
+  const countConseillers = await dbDatalake.collection('conseillers').countDocuments({});
   const total = countConseillersSupprimes + countConseillers;
 
   await dbDatalake.collection('conseillersTotal').updateOne(
