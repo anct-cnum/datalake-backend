310a4f7b6513f1947884df489e4fc19d4ef426cb
morgan <morgan@fitlab.fr>
2023-08-03 17:14:40 +0200

    Suppression de ligne suite à la suppression de la requete

--- src/tasks/stats/cras/crasByDepartements/tasks/index.js
+++ src/tasks/stats/cras/crasByDepartements/tasks/index.js
@@ -2,7 +2,6 @@
 module.exports = {
   ...require('./accompagnementsTotalParDepartement'),
   ...require('./usagersTotalParDepartement'),
-  ...require('./usagersUniquesParDepartement'),
   ...require('./statutsParDepartement'),
   ...require('./agesParDepartement'),
   ...require('./themesParDepartement'),
