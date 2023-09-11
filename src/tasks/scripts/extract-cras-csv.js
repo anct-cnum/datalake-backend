783699631f49b202b55001ac6ba653ad9edf001c
morgan <morgan@fitlab.fr>
2023-09-11 11:36:05 +0200

    modification pour cursor

--- src/tasks/scripts/extract-cras-csv.js
+++ src/tasks/scripts/extract-cras-csv.js
@@ -7,25 +7,21 @@ const { execute } = require('../utils');
 
 require('dotenv').config();
 
-// Penser à réaliser la commande avec --max-old-space-size=8192
-// pour traiter la quantité de données sans problème de mémoire
 execute(__filename, async ({ logger, dbDatalake }) => {
   program.helpOption('-e', 'HELP command');
   program.parse(process.argv);
 
-  const cras = await dbDatalake.collection('cras').find().toArray();
-  const promises = [];
+  const cursor = await dbDatalake.collection('cras').find();
+
   logger.info(`Generating CSV file...`);
   let csvFile = path.join(__dirname, '../../../data/exports', `cras.csv`);
   let file = fs.createWriteStream(csvFile, { flags: 'w' });
 
-  file.write(`ID,ConseillerId,Cra,CreatedAt,UpdatedAt,PermanenceId,StructureId\n`);
-  cras.forEach(cra => {
-    promises.push(new Promise(async resolve => {
-      file.write(`${cra._id},${cra.conseillerId},${cra.cra},${cra.createdAt},${cra.updatedAt},${cra.permanenceId},${cra.structureId},\n`);
-      resolve();
-    }));
-  });
-  await Promise.all(promises);
+  file.write(`ID;ConseillerId;Cra;CreatedAt;UpdatedAt;PermanenceId;StructureId\n`);
+  while (await cursor.hasNext()) {
+    const cra = await cursor.next();
+    file.write(`${cra._id};${cra.conseillerId};${JSON.stringify(cra.cra)};${cra.createdAt};${cra.updatedAt};${cra.permanenceId};${cra.structureId};\n`);
+  }
+
   file.close();
 });
