884757ad6926332b17573eafa239899f182ac4ad
Ornella <68587983+Ornella452@users.noreply.github.com>
2024-11-08 15:22:40 +0100

    stopped cron 15 novembre (#104)

--- src/tasks/jobs/mongodb-sync.sh
+++ src/tasks/jobs/mongodb-sync.sh
@@ -6,7 +6,6 @@ echo "MongoDB database synchronization : START\n"
 node src/tasks/mongodbSync/conseillers
 node src/tasks/mongodbSync/conseillersSupprimes
 node src/tasks/mongodbSync/countTotalConseillers
-node src/tasks/mongodbSync/cras
 node src/tasks/mongodbSync/structures
 node src/tasks/mongodbSync/users
 echo "MongoDB database synchronization : END\n"
